47742997776c37c59b0b98ebb7c28323d2e7c233
pranjaj011 <49923043+pranjaj011@users.noreply.github.com>
2019-05-27 22:47:33 -0700

    Create months.py

---
+++ months.py
@@ -0,0 +1,3 @@
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+for x in months:
+    print("One of the months of the year is", x)
